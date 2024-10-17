e00f4aca1b923ce0a0aad9a6a4113b23c80fb9c1
developeragency5 <Developer.agency@yahoo.com>
2024-10-18 05:26:56 +0530

    git push

--- src/pages/CustomerService.jsx
+++ src/pages/CustomerService.jsx
@@ -11,7 +11,7 @@ const CustomerService = () => {
     <div className="customer-service-container" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="content">
         <h3>Looking for more than just a repair?</h3>
-        <div className="yellow-line"></div>
+        
         <p>
         We investigate and fix equipment and programming issues, upgrade execution, and give arrangement and upkeep administrations to the two computers and tablets.
         </p>
@@ -25,7 +25,7 @@ const CustomerService = () => {
           <p>✔ Get Device Repaired</p>
         </div>
         <div>
-            <p>
+            <p className='mc'>
             InkJetCare is an autonomous outsider, IT specialist organization for programming related issues. Except if expressed, we are not associated with any organization, association, maker, ISP, or email supplier. Our administrations have assisted a huge number of individuals with their PC issues. These administrations may likewise be accessible by the brand proprietor. 
             </p>
         </div>
